9728eaaec9f937ce07185ca3cada554c655b18f9
Jhonatan Catabriga <jhonatancatabriga@gmail.com>
2014-03-16 21:48:05 -0300

    ajustes

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ var path = require('path');
 var io = require('socket.io').listen(server);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.set('port', port);
+app.set('ip', ipaddr);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -47,7 +50,7 @@ io.sockets.on('connection', function (socket) {
 
 });
 
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port') );
+server.listen(app.get('port'),app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port') + ', ip '+app.get('ip'));
 });
 
